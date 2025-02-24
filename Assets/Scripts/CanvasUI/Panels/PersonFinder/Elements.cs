f13295a311dc9062ab154988eb5adf5ecab911af
Koresh01 <kmatveev2004@gmail.com>
2025-02-24 14:17:48 +0300

    Панель поиска "ДЕМО"

---
+++ Assets/Scripts/CanvasUI/Panels/PersonFinder/Elements.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace PrYFam
+{
+    /// <summary>
+    /// Класс позволяет получить доступ к основным элементам префаба карточки члена семьи,
+    /// которая лежит в vertical Scroll view.
+    /// </summary>
+    class Elements : MonoBehaviour
+    {
+        [Tooltip("Контейнер для фамилии.")]
+        public TextMeshProUGUI lastName;
+
+        [Tooltip("Контейнер для имени.")]
+        public TextMeshProUGUI name;
+
+        [Tooltip("Контейнер для отчества.")]
+        public TextMeshProUGUI middleName;
+
+        [Tooltip("Контейнер для аватарки.")]
+        public Image ProfilePicture;
+
+        [Tooltip("Ссылка непосредственно на сам объект:")]
+        public Member member;
+    }
+}
---
+++ Assets/Scripts/CanvasUI/Panels/PersonFinder/PersonFinder.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace PrYFam
+{
+    /// <summary>
+    /// Скрипт висит на панели поиска членов семейного древа.
+    /// Отображает в scrollView всех членов, у которых встречается слово "word";
+    /// </summary>
+    class PersonFinder : MonoBehaviour
+    {
+        [Tooltip("Объект на сцене, хранящий все карточки.")]
+        [SerializeField] private GameObject cardsPlaceholder;
+
+        [Tooltip("Ключевое слово, по которому ищем.")]
+        [SerializeField] private TMP_InputField word;
+
+        [Tooltip("Контейнер элементов вертикального списка.")]
+        [SerializeField] private Transform container;
+
+        [Tooltip("Префаб -> FoundedPerson(найденный член семьи).")]
+        [SerializeField] private GameObject prefab;
+
+        /// <summary>
+        /// Находит и выводит нужные карточки.
+        /// </summary>
+        public void FindByWord()
+        {
+            Clear(); // Очистка предыдущих результатов
+            List<Member> members = GetCardsInfos();
+
+            foreach (Member m in members)
+            {
+                string context = MemberToString(m);
+                if (WordExistsInContext(context))
+                {
+                    // Создаем новый объект карточки и добавляем в контейнер
+                    GameObject newCard = Instantiate(prefab, container);
+                    
+                    // Можно добавить настройку newCard, например, установить текст
+                    Elements elements = newCard.GetComponent<Elements>();
+                    
+                    elements.name.text = m.FirstName;
+                    elements.lastName.text = m.LastName;
+                    elements.middleName.text = m.MiddleName;
+                    elements.ProfilePicture.sprite = m.ProfilePicture;
+
+                    elements.member = m;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Получает все карточки дочерних элементов.
+        /// </summary>
+        private List<Member> GetCardsInfos()
+        {
+            // Получаем все компоненты Member из дочерних объектов
+            Member[] members = cardsPlaceholder.GetComponentsInChildren<Member>();
+            return new List<Member>(members);
+        }
+
+        /// <summary>
+        /// Проверяет содержится ли слово в данном контексте.
+        /// </summary>
+        /// <param name="context">Весь текст, который встречается у конкретного человека( из скрипта Member)</param>
+        private bool WordExistsInContext(string context)
+        {
+            if (string.IsNullOrEmpty(word.text))    // если пользователь ничего не ввел, то выводим все карточки.
+                return true;
+            else if (context.ToLower().Contains(word.text.ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Переводит всю meta-информацию о человеке в строку.
+        /// </summary>
+        private string MemberToString(Member m)
+        {
+            return string.Join(" ", new string[] { m.FirstName, m.LastName, m.MiddleName, m.DateOfBirth, m.PlaceOfBirth, m.Biography });
+        }
+
+        /// <summary>
+        /// Удаляет карточки из контейнера - vertical scrollView;
+        /// </summary>
+        public void Clear()
+        {
+            foreach (Transform child in container)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
